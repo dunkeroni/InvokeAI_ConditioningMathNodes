6c73a60e93e98ca4dc20109e77f9719bf5ac8b07
Kevin Turner <566360-keturn@users.noreply.gitlab.com>
2025-05-21 13:27:41 -0700

    fix: fix optional fields for Invoke 5.12

--- conditioning_math.py
+++ conditioning_math.py
@@ -1,4 +1,4 @@
-from typing import Literal, Protocol
+from typing import Literal, Protocol, Optional
 
 import numpy as np
 import sympy
@@ -116,9 +116,8 @@ class ConditioningMathInvocation(BaseInvocation):
         input=Input.Connection, #A is required for extra information in some operations
         ui_order=0,
     )
-    b: ConditioningField = InputField(
+    b: Optional[ConditioningField] = InputField(
         description="Conditioning B",
-        default=None,
         ui_order=1,
     )
     alpha: float = InputField(
@@ -189,9 +188,8 @@ class FluxConditioningMathInvocation(ConditioningMathInvocation):
         input=Input.Connection, #A is required for extra information in some operations
         ui_order=0,
     )
-    b: FluxConditioningField = InputField(
+    b: Optional[FluxConditioningField] = InputField(
         description="Conditioning B",
-        default=None,
         ui_order=1,
     )
 
@@ -376,9 +374,8 @@ class CogView4ConditioningMathInvocation(ConditioningMathInvocation):
         input=Input.Connection, #A is required for extra information in some operations
         ui_order=0,
     )
-    b: CogView4ConditioningField = InputField(
+    b: Optional[CogView4ConditioningField] = InputField(
         description="Conditioning B",
-        default=None,
         ui_order=1,
     )
 
